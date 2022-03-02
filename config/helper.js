9261327ef2f28a1b057369a3a67b132064f5b9be
Segun <iyandaoluwasegun7@gmail.com>
2022-03-02 16:26:01 +0100

    installed the underscore package

--- config/helper.js
+++ config/helper.js
@@ -1,10 +1,12 @@
+const _ = require("underscore");
+
 exports.testDB = async (db) => {
-    try {
-        await db.authenticate();
-        console.log("Connection has been established successfully.");
-    } catch (error) {
-        console.error("Unable to connect to the database:", error);
-    }
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 };
 
 exports.generateUniqueCode = (size = 6, alpha = true) => {
