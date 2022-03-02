924be045f6c5c070ea12e31b3ee56d654bdc6405
Segun <iyandaoluwasegun7@gmail.com>
2022-03-02 15:12:59 +0100

    helper endpoints implementation

---
+++ config/helper.js
@@ -0,0 +1,30 @@
+exports.testDB = async (db) => {
+    try {
+        await db.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+};
+
+exports.generateUniqueCode = (size = 6, alpha = true) => {
+  let characters = alpha
+    ? "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    : "0123456789";
+  characters = characters.split("");
+  let selections = "";
+  for (let i = 0; i < size; i++) {
+    let index = Math.floor(Math.random() * characters.length);
+    selections += characters[index];
+    characters.splice(index, 1);
+  }
+  return selections;
+};
+
+exports.validateParams = (req, next, request) => {
+  request.map((item) => {
+    if (!req.body[item]) throw new Error(`${item} is required`);
+  });
+  let data = _.pick(req.body, request);
+  return data;
+};
